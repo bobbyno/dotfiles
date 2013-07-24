e5388fbedb5746fd73094a76b90cab5f20d06415
Bobby Norton <bobby@getsetlearning.com>
2013-07-24 08:56:19 -0500

    Add emacs theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,7 @@
                       starter-kit-bindings
                       starter-kit-eshell
                       exec-path-from-shell
+                      zenburn-theme
                       clojure-mode
                       clojure-test-mode
                       nrepl))
@@ -16,4 +17,24 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+(load-theme 'zenburn t)
+
 (server-start)
+
+(defun set-mac-font (name size)
+  (interactive
+   (list (completing-read "font-name: "
+                          (mapcar (lambda (p) (list (car p) (car p)))
+                                  (font-family-list)) nil t)
+         (read-number "size: " 12)))
+  (set-face-attribute 'default nil
+                      :family name
+                      :slant  'normal
+                      :weight 'normal
+                      :width  'normal
+                      :height (* 10 size))
+  (frame-parameter nil 'font))
+
+(set-mac-font "Deja Vu Sans Mono" 17)
+
+(setq-default ispell-program-name "aspell")
