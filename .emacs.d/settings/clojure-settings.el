c4b9aa4345abba0fe802bce3f5b1832f3c515e65
Bobby Norton <bobby@testedminds.com>
2015-06-08 09:38:20 -0500

    Correct keybindings in tmux; add duplicate-line to paredit settings

--- .emacs.d/settings/clojure-settings.el
+++ .emacs.d/settings/clojure-settings.el
@@ -157,46 +157,48 @@
 (defun esk-prog-mode-hook ()
   (run-hooks 'prog-mode-hook))
 
-  (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-  (add-hook 'emacs-lisp-mode-hook 'esk-remove-elc-on-save)
-  (add-hook 'emacs-lisp-mode-hook 'esk-prog-mode-hook)
-  (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'esk-remove-elc-on-save)
+(add-hook 'emacs-lisp-mode-hook 'esk-prog-mode-hook)
+(add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
 
-  (defun esk-remove-elc-on-save ()
-    "If you're saving an elisp file, likely the .elc is no longer valid."
-    (make-local-variable 'after-save-hook)
-    (add-hook 'after-save-hook
-              (lambda ()
-                (if (file-exists-p (concat buffer-file-name "c"))
-                    (delete-file (concat buffer-file-name "c"))))))
+(defun esk-remove-elc-on-save ()
+  "If you're saving an elisp file, likely the .elc is no longer valid."
+  (make-local-variable 'after-save-hook)
+  (add-hook 'after-save-hook
+            (lambda ()
+              (if (file-exists-p (concat buffer-file-name "c"))
+                  (delete-file (concat buffer-file-name "c"))))))
 
-  (define-key emacs-lisp-mode-map (kbd "C-c v") 'eval-buffer)
+(define-key emacs-lisp-mode-map (kbd "C-c v") 'eval-buffer)
 
 ;;; Enhance Lisp Modes
 
-  (define-key read-expression-map (kbd "TAB") 'lisp-complete-symbol)
-  (define-key lisp-mode-shared-map (kbd "RET") 'reindent-then-newline-and-indent)
+(define-key read-expression-map (kbd "TAB") 'lisp-complete-symbol)
+(define-key lisp-mode-shared-map (kbd "RET") 'reindent-then-newline-and-indent)
 
-  ;; TODO: look into parenface package
-  (defface esk-paren-face
-    '((((class color) (background dark))
-       (:foreground "grey50"))
-      (((class color) (background light))
-       (:foreground "grey55")))
-    "Face used to dim parentheses."
-    :group 'starter-kit-faces)
+;; TODO: look into parenface package
+(defface esk-paren-face
+  '((((class color) (background dark))
+     (:foreground "grey50"))
+    (((class color) (background light))
+     (:foreground "grey55")))
+  "Face used to dim parentheses."
+  :group 'starter-kit-faces)
 
-  (eval-after-load 'paredit
-    ;; need a binding that works in the terminal
-    '(progn
-       (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
-       (define-key paredit-mode-map (kbd "M-(") 'paredit-backward-slurp-sexp)))
+(eval-after-load 'paredit
+  ;; need a binding that works in the terminal
+  '(progn
+     (define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp)
+     (define-key paredit-mode-map (kbd "M-(") 'paredit-backward-slurp-sexp)
+     (define-key paredit-mode-map (kbd "M-d") 'duplicate-line)))
 
-  (dolist (mode '(scheme emacs-lisp lisp clojure clojurescript))
-    (when (> (display-color-cells) 8)
-      (font-lock-add-keywords (intern (concat (symbol-name mode) "-mode"))
-                              '(("(\\|)" . 'esk-paren-face))))
-    (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
-              'paredit-mode))
+(dolist (mode '(scheme emacs-lisp lisp clojure clojurescript))
+  (when (> (display-color-cells) 8)
+    (font-lock-add-keywords (intern (concat (symbol-name mode) "-mode"))
+                            '(("(\\|)" . 'esk-paren-face))))
+  (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
+            'paredit-mode))
 
 (provide 'clojure-settings)
+
