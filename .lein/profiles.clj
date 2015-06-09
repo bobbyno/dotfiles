9800037b4c1c94156fcd6179387eac16467e5fbe
Bobby Norton <bobby@testedminds.com>
2015-06-09 14:22:50 -0500

    Remove android libs

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,10 +4,7 @@
         :repl-options
         {:init
          (do
-           (set! *print-length* 100))}
-        :android {:sdk-path "/usr/local/Cellar/android-sdk/23.0.2"}}
- :android-config {:android {:sdk-path "/usr/local/Cellar/android-sdk/23.0.2"}}}
-                  ;; [lein-droid "0.2.4-SNAPSHOT"]
+           (set! *print-length* 100))}}}
                   ;; [lein-describe "0.3.0-SNAPSHOT"]
                   ;; [lein-difftest "2.0.0"]
                   ;; [lein-pprint "1.1.1"]
