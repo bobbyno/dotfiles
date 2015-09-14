ec9478a69c7c7262162eb6f7621d7087c562d366
Bobby Norton <bobby@testedminds.com>
2015-09-14 15:45:30 -0500

    Update lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]]
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+                  [lein-try "0.4.3"]
+                  [lein-exec "0.3.5"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
         :repl-options
         {:init
@@ -9,7 +11,4 @@
 ;; [lein-difftest "2.0.0"]
 ;; [lein-pprint "1.1.1"]
 ;; [compojure/lein-template "0.3.0"]
-;; [lein-try "0.4.1"]
-;; [lein-clique "0.1.2"]
 ;; [com.jakemccrary/lein-test-refresh "0.3.0"]
-;; [org.clojars.kurtharriger/lein-depgraph "0.1.0"]
